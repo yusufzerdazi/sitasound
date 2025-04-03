ea829ec668f0084a7de9c7d9d233f45267f8883d
Yusuf Zerdazi <yusuf@zerdazi.com>
2025-04-03 13:46:53 +0100

    Updates to visuals

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import { HashRouter, Link, Route, Routes } from "react-router-dom"
-import Home from "./Home"
-import Events from "./Events"
-import Art from "./Art"
-import DJs from "./DJs"
 
 function App() {
   return (
-    <>
-      <div className="p-5 flex flex-col items-center justify-center">
-        <div className='pb-10'>
-          <img className="max-w-80 w-full" src="sitasound.svg"></img>
-        </div>
+    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+      <div className='pb-10'>
+        <img className="max-w-80 w-full" src="sitasound.jpg" alt="Sita Sound" />
       </div>
-    </>
+      <h1 className="coming-soon">Coming Soon</h1>
+    </div>
   )
 }
+
 export default App
